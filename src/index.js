a005d0e478254ca3256701a35a980035db0ea9f8
ViktorK66 <55109336+ViktorK66@users.noreply.github.com>
2019-10-18 23:39:06 +0300

    feat: add solution sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  function Validation(matrix, row, col, item) {
+    for (let i = 0; i < 9; i++) {
+      const f = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+      const g = 3 * Math.floor(col / 3) + i % 3;
+      if (matrix[row][i] === item || matrix[i][col] === item || matrix[f][g] === item) return false;
+    }
+    return true;
+  }
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (matrix[i][j] === 0) {
+        for (let item = 1; item <= 9; item++) {
+          if (Validation(matrix, i, j, item)) {
+            matrix[i][j] = item;
+            if (solveSudoku(matrix)) return matrix;
+            else matrix[i][j] = 0;
+          }
+        }
+      return false;
+      }
+    }
+  }
+  return matrix;
 }
